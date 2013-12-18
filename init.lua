3fb020cfdaa0843b45ab393f1a5f4d1d46ece478
root <root@localhost.(none)>
2013-12-19 03:17:57 +0800

    upload files webshell

--- init.lua
+++ init.lua
@@ -33,15 +33,23 @@ function write(logfile,msg)
 end
 function log(method,url,data,ruletag)
     if attacklog then
+	    local post_data_log
 	    local realIp = getClientIp()
-	    local ua = ngx.var.http_user_agent
-    	local servername=ngx.var.server_name
+	local request_url="http://"..ngx.var.host..ngx.var.request_uri
+	local a_rule=ruletag
+	local servername=ngx.var.server_name
     	local time=ngx.localtime()
-	    if ua  then
-		    line = realIp.." ["..time.."] \""..method.." "..servername..url.."\" \""..data.."\"  \""..ua.."\" \""..ruletag.."\"\n"
-    	else
-	    	line = realIp.." ["..time.."] \""..method.." "..servername..url.."\" \""..data.."\" - \""..ruletag.."\"\n"
-    	end
+	local receive_headers = ngx.req.get_headers()
+	a_rule=string.gsub(a_rule,"\\","")
+        if string.sub(receive_headers["content-type"],1,20) == "multipart/form-data;" then
+	post_data_log="multipart/form-data"
+	else
+        post_data_log = ngx.req.get_body_data()
+        end
+        if post_data_log==nil then
+		post_data_log=''
+	end
+		    line = realIp.." ["..time.."] \""..method.." "..request_url.."\" \"".."post_data:"..post_data_log.."\"  \""..a_rule.."\"\n"
 	    local filename = logpath..'/'..servername.."_"..ngx.today().."_sec.log"
         write(filename,line)
     end
@@ -66,6 +74,7 @@ uarules=read_rule('user-agent')
 wturlrules=read_rule('whiteurl')
 postrules=read_rule('post')
 ckrules=read_rule('cookie')
+extrules=read_rule('ext')
 
 
 function say_html()
@@ -99,7 +108,7 @@ function args()
                 data=val
             end
             if data and type(data) ~= "boolean" and rule ~="" and ngxmatch(unescape(data),rule,"isjo") then
-				log('GET',ngx.var.request_uri,"-",rule)
+				log('GET',ngx.var.host,"-",rule)
                 say_html()
                 return true
             end
@@ -220,4 +229,171 @@ function blockip()
          end
      end
          return false
+end
+function check_ext(filename)
+if filename==nil then
+return true
+end
+if filename=="" then
+return true
+end
+if type(filename)~="string" then
+return true
+end
+local check_have_ext,_,_,_=string.find(filename,'%.')
+if check_have_ext==nil then
+return true
+end
+local ext=filename:match(".+%.(%w*)$")
+            for _,rule in pairs(extrules) do
+                if rule ~="" and ngxmatch(ext,rule,"isjo") then
+                    log('EXT_upload',ngx.var.request_uri,"-","file_name:"..ext.."--reg:"..rule)
+                    say_html()
+                return true
+                end
+            end
+--var_dump(ext)
+local t =explode(filename,'.')
+for aa,vv in pairs(t) do
+if aa ~=1 then
+            for _,rule in pairs(extrules) do
+                if rule ~="" and ngxmatch(vv,rule,"isjo") then
+                    log('EXT_upload',ngx.var.request_uri,"-","file_name:"..vv.."--reg:"..rule)
+                    say_html()
+                return true
+                end
+            end
+
+end
+end
+--var_dump(t)
+--ngx.exit(200)
+
+end
+
+
+
+
+    function var_dump(data, max_level, prefix)   
+        if type(prefix) ~= "string" then   
+            prefix = ""  
+        end   
+        if type(data) ~= "table" then   
+            dump_html(prefix .. tostring(data))   
+        else  
+            dump_html(data)   
+            if max_level ~= 0 then   
+                local prefix_next = prefix .. "    "  
+                dump_html(prefix .. "{")   
+                for k,v in pairs(data) do  
+                    io.stdout:write(prefix_next .. k .. " = ")   
+                    if type(v) ~= "table" or (type(max_level) == "number" and max_level <= 1) then   
+                        dump_html(v)   
+                    else  
+                        if max_level == nil then   
+                            var_dump(v, nil, prefix_next)   
+                        else  
+                            var_dump(v, max_level - 1, prefix_next)   
+                        end   
+                    end   
+                end   
+                dump_html(prefix .. "}")   
+            end   
+        end   
+    end  
+
+
+function dump_html(str)
+    if Redirect then
+        --ngx.header.content_type = "text/html"
+        ngx.say(str)
+    end
+end
+
+
+function explode ( _str,seperator )  
+    local pos, arr = 0, {}  
+        for st, sp in function() return string.find( _str, seperator, pos, true ) end do  
+            table.insert( arr, string.sub( _str, pos, st-1 ) )  
+            pos = sp + 1  
+        end  
+    table.insert( arr, string.sub( _str, pos ) )  
+    return arr  
+end 
+
+
+function post_check()
+local args = {}
+local file_args = {}
+local is_have_file_param = false
+local receive_headers = ngx.req.get_headers()
+local request_method = ngx.var.request_method
+if request_method == "POST" then
+ngx.req.read_body()
+if string.sub(receive_headers["content-type"],1,20) == "multipart/form-data;" then--判断是否是multipart/form-data类型的表单
+is_have_file_param = true
+content_type = receive_headers["content-type"]
+body_data = ngx.req.get_body_data()--body_data可是符合http协议的请求体，不是普通的字符串
+--请求体的size大于nginx配置里的client_body_buffer_size，则会导致请求体被缓冲到磁盘临时文件里，client_body_buffer_size默认是8k或者16k
+if not body_data then
+  local datafile = ngx.req.get_body_file()
+  if not datafile then
+       error_code = 1
+       error_msg = "no request body found"
+  else
+       local fh, err = io.open(datafile, "r")
+  if not fh then
+       error_code = 1
+       error_msg = "failed to open " .. tostring(datafile) .. "for reading: " .. tostring(err)
+  else
+       fh:seek("set")
+       body_data = fh:read("*a")
+       fh:close()
+  if body_data == "" then
+       error_code = 1
+       error_msg = "request body is empty"
+  end
+  end
+  end
+  end
+if error_code==1 then
+return true
+end
+local boundary = "--" .. string.sub(receive_headers["content-type"],31)
+local body_data_table = explode(tostring(body_data),boundary)
+local first_string = table.remove(body_data_table,1)
+local last_string = table.remove(body_data_table)
+for i,v in ipairs(body_data_table) do
+    local start_pos,end_pos,capture,capture2 = string.find(v,'Content%-Disposition: form%-data; name="(.+)"; filename="(.-)"')
+    if not start_pos then--普通参数
+        local t = explode(v,"\r\n\r\n")
+        local temp_param_name = string.sub(t[1],41,-2)
+        body(temp_param_name)
+	local temp_param_value = string.sub(t[2],1,-3)
+        body(temp_param_value)
+    else --文件类型的参数，capture是参数名称，capture2是文件名
+        check_ext(capture2)
+    end
+end
+else
+local args_post = ngx.req.get_post_args()
+    for _,rule in pairs(argsrules) do
+    local args_post = ngx.req.get_post_args()
+        for key, val in pairs(args_post) do
+            if type(val)=='table' then
+                data=table.concat(val, " ")
+            else
+                data=val
+            end
+            if data and type(data) ~= "boolean" and rule ~="" and ngxmatch(unescape(data),rule,"isjo") then
+                                log('POST',ngx.var.host,"-",rule)
+                say_html()
+                return true
+            end
+        end
+    end
+
+end
+end
+return true
 end
--- waf.lua
+++ waf.lua
@@ -1,70 +1,18 @@
 local content_length=tonumber(ngx.req.get_headers()['content-length'])
+--ngx.req.read_body()
 local method=ngx.req.get_method()
-if whiteip() then
-elseif blockip() then
-elseif denycc() then
-elseif ngx.var.http_Acunetix_Aspect then
+whiteip()
+blockip()
+denycc()
+if ngx.var.http_Acunetix_Aspect then
     ngx.exit(444)
-elseif ngx.var.http_X_Scan_Memo then
-    ngx.exit(444)
-elseif whiteurl() then
-elseif ua() then
-elseif url() then
-elseif args() then
-elseif cookie() then
-elseif PostCheck then
-    if method=="POST" then   
-            local boundary = get_boundary()
-	    if boundary then
-	    local len = string.len
-            local sock, err = ngx.req.socket()
-    	    if not sock then
-					return
-            end
-	    ngx.req.init_body(128 * 1024)
-            sock:settimeout(0)
-	    local content_length = nil
-    	    content_length=tonumber(ngx.req.get_headers()['content-length'])
-    	    local chunk_size = 4096
-            if content_length < chunk_size then
-					chunk_size = content_length
-	    end
-            local size = 0
-	    while size < content_length do
-		local data, err, partial = sock:receive(chunk_size)
-		data = data or partial
-		if not data then
-			return
-		end
-		ngx.req.append_body(data)
-		size = size + len(data)
-		local less = content_length - size
-		if less < chunk_size then
-			chunk_size = less
-		end
-            end
-    	    	ngx.req.finish_body()
-        	if body(data) then
-	   	        return true
-    	    	end
-    else
-			ngx.req.read_body()
-			local args = ngx.req.get_post_args()
-			if not args then
-				return
-			end
-			for key, val in pairs(args) do
-				if type(val) == "table" then
-					data=table.concat(val, ", ")
-				else
-					data=val
-				end
-				if data and type(data) ~= "boolean" and body(data) then
-                  return true
-				end
-			end
-		end
-    end
-else
-    return
 end
+if ngx.var.http_X_Scan_Memo then
+    ngx.exit(444)
+end
+whiteurl()
+ua()
+url()
+args()
+cookie()
+post_check()
